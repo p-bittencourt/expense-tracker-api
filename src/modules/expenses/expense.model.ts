50294ade4de70964e0c3c18fc9a597980da5abc2
pedro <pedromonteirobittencourt@gmail.com>
2024-11-06 18:22:40 -0300

    Add Expense model and enum for expense types

---
+++ src/modules/expenses/expense.model.ts
@@ -0,0 +1,30 @@
+import mongoose, { Schema, Document, ObjectId } from 'mongoose';
+import { ExpenseType } from './expense-type.enum';
+
+const ExpenseSchema: Schema = new Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: 'Users' },
+    title: { type: String, required: true },
+    cost: { type: Number, required: true },
+    type: {
+      type: String,
+      enum: Object.values(ExpenseType),
+      required: true,
+      default: ExpenseType.OTHER,
+    },
+  },
+  { timestamps: true }
+);
+
+export interface IExpense extends Document {
+  id: ObjectId;
+  userId: ObjectId;
+  title: string;
+  cost: number;
+  type: ExpenseType;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const Expense = mongoose.model<IExpense>('Expense', ExpenseSchema);
+export default Expense;
